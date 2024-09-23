b589d28c716705564f49cac2acced0f648385a9b
Sandeep Kumar Jaiswal <jaiswalsandeep508@gmail.com>
2024-09-23 15:47:11 +0530

    I have add folder collections in which add a file ArrayList I have learn about how to use Collection ArrayList

---
+++ Collections/ArrayList.java
@@ -0,0 +1,19 @@
+package Collections;
+
+import java.util.Collection;
+
+public class ArrayList {
+  public static void main(String[] args) {
+    Collection<Integer> nums = new java.util.ArrayList<Integer>();
+    nums.add(5);
+    nums.add(6);
+    nums.add(7);
+    nums.add(8);
+    nums.add(9);
+    nums.add(10);
+
+    for (int i : nums) {
+      System.out.println(i * 2);
+    }
+  }
+}
