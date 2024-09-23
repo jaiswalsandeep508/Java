bb0df00223440818ea96d234c455b25fa8d8b594
Sandeep Kumar Jaiswal <jaiswalsandeep508@gmail.com>
2024-09-23 16:20:50 +0530

    Add folder TreeSet in which I learn about Treeset which is return a sorted with unique values of collection

---
+++ Collections/TreeSet.java
@@ -0,0 +1,19 @@
+package Collections;
+
+import java.util.Set;
+
+public class TreeSet {
+  public static void main(String[] args) {
+    Set<Integer> nums = new java.util.TreeSet<Integer>();
+    nums.add(63);
+    nums.add(45); // TreeSet return a sorted array with unique values
+    nums.add(77);
+    nums.add(38);
+    nums.add(77);// remove this element in output due to dublicacy
+    nums.add(17);
+
+    for (int i : nums) {
+      System.out.println(i);
+    }
+  }
+}
